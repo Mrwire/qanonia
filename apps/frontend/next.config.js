ce639f9b8cb4d6f8b47a6715b3882977f41574dd
Oussama BENAAMIMI <35456006+Mrwire@users.noreply.github.com>
2025-09-21 15:15:23 +0100

    Add Next.js configuration with next-intl plugin and transpile packages

---
+++ apps/frontend/next.config.js
@@ -0,0 +1,16 @@
+const withNextIntl = require('next-intl/plugin')('./src/i18n/request.ts');
+
+module.exports = withNextIntl({
+  experimental: {
+    typedRoutes: true,
+  },
+  transpilePackages: ['@qanounia/ui', '@qanounia/utils'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+});
